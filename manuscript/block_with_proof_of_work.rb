9f1c1e485e2e7d5871408650e6ba7ef06dc21fbc
mattb20 <matt_burgoyne@live.co.uk>
2018-06-13 19:48:38 +0100

    0000 difficulty attempted

--- manuscript/block_with_proof_of_work.rb
+++ manuscript/block_with_proof_of_work.rb
@@ -12,7 +12,7 @@ class Block
     @nonce, @hash  = compute_hash_with_proof_of_work
   end
 
-  def compute_hash_with_proof_of_work( difficulty='00' )
+  def compute_hash_with_proof_of_work( difficulty='0000' )
     nonce = 0
     loop do
       hash = Digest::SHA256.hexdigest( "#{nonce}#{data}" )
@@ -45,14 +45,4 @@ pp Block.new( <<TXT )
 TXT
 
 
-p Digest::SHA256.hexdigest( "0Hello, Cryptos!" )
-p Digest::SHA256.hexdigest( "1Hello, Cryptos!" )
-p Digest::SHA256.hexdigest( "2Hello, Cryptos!" )
-
-p Digest::SHA256.hexdigest( "143Hello, Cryptos!" )
-
-
-p Digest::SHA256.hexdigest( '26762Hello, Cryptos!' )
-p Digest::SHA256.hexdigest( '68419Hello, Cryptos! - Hello, Cryptos! - Hello, Cryptos!' )
-p Digest::SHA256.hexdigest( '23416Your Name Here' )
-p Digest::SHA256.hexdigest( '15353Data Data Data Data' )
+pp Digest::SHA256.hexdigest( '26762Hello, Cryptos!' )
